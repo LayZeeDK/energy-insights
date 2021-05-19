069d15146650ea79d81bbf51dece4e4b3948c545
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-05-19 22:49:32 +0200

    feat: load records every minute and initially

--- libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@angular/core';
-import { ComponentStore } from '@ngrx/component-store';
-import { Observable } from 'rxjs';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
+import { combineLatest, Observable, timer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
+import { Co2EmissionPrognosisHttp } from './co2-emission-prognosis-http.service';
 import { Co2EmissionPrognosisRecords } from './co2-emission-prognosis-record';
 
 export interface Co2ForecastState {
   readonly records: Co2EmissionPrognosisRecords;
+}
+
+interface QueryFilter {
+  readonly from: Date;
+  readonly to: Date;
 }
 
 @Injectable()
@@ -14,9 +21,34 @@ export class Co2ForecastStore extends ComponentStore<Co2ForecastState> {
     state => state.records
   );
 
-  constructor() {
+  constructor(private http: Co2EmissionPrognosisHttp) {
     super(initialState);
+
+    this.loadRecordsEveryMinute({
+      from: new Date(),
+      to: new Date(),
+    });
   }
+
+  private loadRecordsEveryMinute = this.effect<QueryFilter>(queryFilter$ =>
+    combineLatest([queryFilter$, timer(0, 60 * 1000)]).pipe(
+      switchMap(queryFilter =>
+        this.http.get().pipe(
+          tapResponse(
+            records => this.updateRecords(records),
+            () => this.updateRecords([])
+          )
+        )
+      )
+    )
+  );
+
+  private updateRecords = this.updater<Co2EmissionPrognosisRecords>(
+    (state, records) => ({
+      ...state,
+      records,
+    })
+  );
 }
 
 const initialState: Co2ForecastState = {
