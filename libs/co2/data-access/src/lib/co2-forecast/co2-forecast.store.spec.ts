067b32db374e9b3ed5d5d168508f87bb5da5c45e
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-05-19 22:22:39 +0200

    feat: add records$

--- libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.spec.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { first } from 'rxjs/operators';
 
 import { Co2ForecastStore } from './co2-forecast.store';
 
@@ -19,5 +20,15 @@ describe(Co2ForecastStore.name, () => {
     const { store } = setup();
 
     expect(store).not.toBeNull();
+  });
+
+  describe('records$', () => {
+    it('initially emits 0 records', async () => {
+      const { store } = setup();
+
+      const records = await store.records$.pipe(first()).toPromise();
+
+      expect(records).toEqual([]);
+    });
   });
 });
--- libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
+import { Observable } from 'rxjs';
 
-import { Co2EmissionPrognosisRecord } from './co2-emission-prognosis-record';
+import { Co2EmissionPrognosisRecords } from './co2-emission-prognosis-record';
 
 export interface Co2ForecastState {
-  readonly records: readonly Co2EmissionPrognosisRecord[];
+  readonly records: Co2EmissionPrognosisRecords;
 }
 
 @Injectable()
 export class Co2ForecastStore extends ComponentStore<Co2ForecastState> {
+  records$: Observable<Co2EmissionPrognosisRecords> = this.select(
+    state => state.records
+  );
+
   constructor() {
     super(initialState);
   }
