e40adeab7c6783217e60e2f38aac72bca6e24eb8
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-05-20 21:38:02 +0200

    refactor: remove export keyword

--- libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.ts
@@ -6,7 +6,7 @@ import { switchMap } from 'rxjs/operators';
 import { Co2EmissionPrognosisHttp } from './co2-emission-prognosis-http.service';
 import { Co2EmissionPrognosisRecords } from './co2-emission-prognosis-record';
 
-export interface Co2ForecastState {
+interface Co2ForecastState {
   readonly records: Co2EmissionPrognosisRecords;
 }
 
