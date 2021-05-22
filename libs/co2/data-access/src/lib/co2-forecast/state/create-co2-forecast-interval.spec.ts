16d7cdddb3cb83f585eaead4b1c192b496ee80a7
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-05-23 00:39:41 +0200

    feat: always display forecast using Danish interval

--- libs/co2/data-access/src/lib/co2-forecast/state/create-co2-forecast-interval.spec.ts
+++ libs/co2/data-access/src/lib/co2-forecast/state/create-co2-forecast-interval.spec.ts
@@ -1,25 +1,30 @@
 import { DateTime } from 'luxon';
 
 import { createCo2ForecastInterval } from './create-co2-forecast-interval';
+import { danishZone } from './danish-zone';
 
 describe(createCo2ForecastInterval.name, () => {
   it('the start of today is the beginning of the forecast', () => {
-    const fakeNow = DateTime.fromISO('2021-05-22T01:46:00+02:00');
+    const fakeNow = DateTime.fromISO('2021-05-22T01:46:00', {
+      zone: danishZone,
+    });
 
     const actualInterval = createCo2ForecastInterval(fakeNow);
 
     expect(actualInterval.start).toEqual(
-      DateTime.fromISO('2021-05-22T00:00:00+02:00')
+      DateTime.fromISO('2021-05-22T00:00:00', { zone: danishZone })
     );
   });
 
   it('the start of the day after tomorrow is the end of the forecast', () => {
-    const fakeNow = DateTime.fromISO('2021-05-22T01:48:00+02:00');
+    const fakeNow = DateTime.fromISO('2021-05-22T01:48:00', {
+      zone: danishZone,
+    });
 
     const actualInterval = createCo2ForecastInterval(fakeNow);
 
     expect(actualInterval.end).toEqual(
-      DateTime.fromISO('2021-05-24T00:00:00+02:00')
+      DateTime.fromISO('2021-05-24T00:00:00+02:00', { zone: danishZone })
     );
   });
 });
--- libs/co2/data-access/src/lib/co2-forecast/state/create-co2-forecast-interval.ts
+++ libs/co2/data-access/src/lib/co2-forecast/state/create-co2-forecast-interval.ts
@@ -1,8 +1,10 @@
 import { DateTime, Duration, Interval } from 'luxon';
 
-export function createCo2ForecastInterval(now: DateTime): Interval {
-  const localToday = now.startOf('day');
-  const localDayAfterTomorrow = localToday.plus(Duration.fromISO('P2D'));
+import { danishZone } from './danish-zone';
 
-  return Interval.fromDateTimes(localToday, localDayAfterTomorrow);
+export function createCo2ForecastInterval(now: DateTime): Interval {
+  const danishToday = now.setZone(danishZone).startOf('day');
+  const danishDayAfterTomorrow = danishToday.plus(Duration.fromISO('P2D'));
+
+  return Interval.fromDateTimes(danishToday, danishDayAfterTomorrow);
 }
