9057df316d43d9a4d86d8f2e68fbcd0e623edb18
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-03-18 00:59:41 +0100

    chore: disable lint rule

--- apps/energy-insights-app/src/app/energy-insights-app.component.ts
+++ apps/energy-insights-app/src/app/energy-insights-app.component.ts
@@ -1,6 +1,8 @@
 import { Component, NgModule } from '@angular/core';
 
 @Component({
+  // The root element is intentionally named after the application
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'energy-insights-app',
   template: `<h1>Energy Insights</h1>`,
   styles: [
