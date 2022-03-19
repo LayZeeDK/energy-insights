d284250db14096fac1c83d02d95104c32b8a84a9
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-03-19 23:45:05 +0100

    refactor: merge root components

--- apps/energy-insights-app/src/app/energy-insights-app.component.ts
+++ apps/energy-insights-app/src/app/energy-insights-app.component.ts
@@ -1,21 +1,79 @@
 import { Component, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Co2FeatureForecastModule } from '@energy-insights/co2/feature-forecast';
 
 @Component({
   // The root element is intentionally named after the application
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'energy-insights-app',
-  template: `<h1>Energy Insights</h1>`,
+  template: `
+    <header class="flex">
+      <img alt="Nx logo" width="75" src="/assets/images/nx-logo-white.svg" />
+      <h1>Welcome to {{ title }}!</h1>
+    </header>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `,
   styles: [
     `
       :host {
         display: block;
+        font-family: sans-serif;
+        min-width: 300px;
+        max-width: 600px;
+        margin: 50px auto;
+      }
+
+      .gutter-left {
+        margin-left: 9px;
+      }
+
+      .col-span-2 {
+        grid-column: span 2;
+      }
+
+      .flex {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      header {
+        background-color: #143055;
+        color: white;
+        padding: 5px;
+        border-radius: 3px;
+      }
+
+      main {
+        padding: 0 36px;
+      }
+
+      p {
+        text-align: center;
+      }
+
+      h1 {
+        text-align: center;
+        margin-left: 18px;
+        font-size: 24px;
+      }
+
+      h2 {
+        text-align: center;
+        font-size: 20px;
+        margin: 40px 0 10px 0;
       }
     `,
   ],
 })
-export class EnergyInsightsAppComponent {}
+export class EnergyInsightsAppComponent {
+  title = 'Energy Insights';
+}
 
 @NgModule({
   declarations: [EnergyInsightsAppComponent],
+  imports: [Co2FeatureForecastModule, RouterModule],
 })
 export class EnergyInsightsAppScam {}
