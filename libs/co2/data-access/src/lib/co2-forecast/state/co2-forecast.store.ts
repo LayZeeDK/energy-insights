36fef390ae23213ce80376d0692a169e8f1830bd
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-03-20 01:06:50 +0100

    refactor(co2): refactor store

--- libs/co2/data-access/src/lib/co2-forecast/state/co2-forecast.store.ts
+++ libs/co2/data-access/src/lib/co2-forecast/state/co2-forecast.store.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { Co2Forecast } from '@energy-insights/co2/domain';
 import { DanishDateStore } from '@energy-insights/co2/util-date-times';
 import { ComponentStore, tapResponse } from '@ngrx/component-store';
-import { DateTime, Duration, Interval } from 'luxon';
-import { combineLatest, Observable, timer } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
+import { Duration, Interval } from 'luxon';
+import { Observable, pipe, timer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 import { Co2EmissionPrognosisHttp } from '../http/co2-emission-prognosis-http.service';
-
-const twoDays = Duration.fromISO('P2D');
 
 interface Co2ForecastState {
   readonly forecast: Co2Forecast;
 }
+
+const twoDays = Duration.fromISO('P2D');
 
 @Injectable()
 export class Co2ForecastStore extends ComponentStore<Co2ForecastState> {
@@ -20,18 +20,20 @@ export class Co2ForecastStore extends ComponentStore<Co2ForecastState> {
 
   constructor(
     private http: Co2EmissionPrognosisHttp,
-    danishDate: DanishDateStore
+    private danishDate: DanishDateStore
   ) {
     super(initialState);
 
-    this.queryForecastEveryMinute(danishDate.today$);
+    const everyMinute$ = timer(0, 60 * 1000);
+    this.#queryForecast(
+      this.select(this.danishDate.today$, everyMinute$, danishToday =>
+        Interval.fromDateTimes(danishToday, danishToday.plus(twoDays))
+      )
+    );
   }
 
-  private queryForecastEveryMinute = this.effect<DateTime>(danishToday$ =>
-    combineLatest([danishToday$, timer(0, 60 * 1000)]).pipe(
-      map(([danishToday]) =>
-        Interval.fromDateTimes(danishToday, danishToday.plus(twoDays))
-      ),
+  #queryForecast = this.effect<Interval>(
+    pipe(
       switchMap(forecastInterval =>
         this.http.get(forecastInterval).pipe(
           tapResponse(
