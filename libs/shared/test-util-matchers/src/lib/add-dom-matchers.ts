828c41b8adfb9fa08e89f26567c28ad56b97d709
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-03-20 13:48:32 +0100

    test: remove unnecessary lint comments

--- libs/shared/test-util-matchers/src/lib/add-dom-matchers.ts
+++ libs/shared/test-util-matchers/src/lib/add-dom-matchers.ts
@@ -1,8 +1,3 @@
 export function addDomMatchers(): void {
-  beforeAll(async () => {
-    // Issue: node_modules/@types/testing-library__jest-dom/index.d.ts' is not a module.
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    await import('@testing-library/jest-dom');
-  });
+  beforeAll(() => import('@testing-library/jest-dom'));
 }
